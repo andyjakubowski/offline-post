2e7d98f9449c893ad28934d70997f07d625cca96
Andy Jakubowski <hello@andyjakubowski.com>
2020-05-19 14:54:54 +0200

    Set up Service Worker caching resources

---
+++ app.js
@@ -0,0 +1,101 @@
+function loadImage(imageJSON) {
+  return new Promise((resolve, reject) => {
+    fetch(imageJSON.url).then((response) => {
+      if (response.status == 200) {
+        const arrayResponse = [];
+        response.blob().then((imageBlob) => {
+          arrayResponse[0] = imageBlob;
+          arrayResponse[1] = imageJSON;
+          resolve(arrayResponse);
+        });
+      } else {
+        reject(
+          Error(
+            `Image did not load successfully; error code: ${response.status} ${response.statusText}`
+          )
+        );
+      }
+    });
+  });
+}
+
+function renderImages() {
+  const images = [
+    {
+      url: "images/sunflower.jpg",
+      alt: "Sunflower",
+      author: "Brigitte Tohm",
+    },
+    {
+      url: "images/icyWaterfall.jpg",
+      alt: "Icy Waterfall",
+      author: "Rémy Penet",
+    },
+    {
+      url: "images/cherryBlossom.jpg",
+      alt: "Cherry Blossom",
+      author: "The Miscellanista",
+    },
+    {
+      url: "images/leaves.jpg",
+      alt: "Leaves",
+      author: "Vino Li",
+    },
+  ];
+  const main = document.querySelector("main");
+  images.forEach((image) => {
+    loadImage(image).then((arrayResponse) => {
+      const figure = document.createElement("figure");
+      const img = document.createElement("img");
+      const figcaption = document.createElement("figcaption");
+      const imageURL = window.URL.createObjectURL(arrayResponse[0]);
+      img.src = imageURL;
+      img.alt = arrayResponse[1].alt;
+      figcaption.innerText = `${arrayResponse[1].alt} by ${arrayResponse[1].author}`;
+      figure.append(img, figcaption);
+      main.append(figure);
+    });
+  });
+}
+
+function runApp() {
+  renderImages();
+}
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/offline-post/serviceWorker.js", {
+      scope: "/offline-post/",
+    })
+    .then((registration) => {
+      console.info(
+        "%cService Worker%c Registered. Scope: ",
+        `
+    color: black;
+    background-color: #FFC857;
+    padding: 0 4px;
+    font-weight: bold;
+  
+    `,
+        "",
+        registration.scope
+      );
+
+      if (registration.installing) {
+        console.log("Service worker installing");
+      } else if (registration.waiting) {
+        console.log("Service worker installed");
+      } else if (registration.active) {
+        console.log("Service worker active");
+      }
+    })
+    .catch((error) => {
+      console.log("Service Worker registration failed:", error);
+    });
+}
+
+if (document.readyState !== "loading") {
+  runApp();
+} else {
+  document.addEventListener("DOMContentLoaded", runApp);
+}
---
+++ serviceWorker.js
@@ -0,0 +1,81 @@
+const path = "/service-worker-demo";
+
+function log(type = "log", message) {
+  const prefix = [
+    "%cService Worker%c",
+    `
+  color: black;
+  background-color: #FFC857;
+  padding: 0 4px;
+  font-weight: bold;
+
+  `,
+    "",
+  ];
+
+  switch (type) {
+    case "info":
+      console.info(...prefix, message);
+      break;
+    default:
+      console.log(...prefix, message);
+  }
+}
+
+function info(message) {
+  log("info", message);
+}
+
+self.addEventListener("install", (event) => {
+  info("install");
+  event.waitUntil(
+    caches.open("v1").then((cache) => {
+      const items = [
+        `${path}/`,
+        `${path}/index.html`,
+        `${path}/styles.css`,
+        `${path}/app.js`,
+        `${path}/images/sunflower.jpg`,
+        `${path}/images/icyWaterfall.jpg`,
+        `${path}/images/cherryBlossom.jpg`,
+        `${path}/images/leaves.jpg`,
+      ];
+      console.log("Opened cache. Adding items:", items);
+      return cache.addAll(items);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  info("activate");
+});
+
+self.addEventListener("message", (event) => {
+  info("message");
+});
+
+self.addEventListener("fetch", (event) => {
+  // I run a local server and live reload the browser with browser-sync
+  // Ignore browser-sync requests
+  if (event.request.url.includes("browser-sync")) {
+    return fetch(event.request);
+  }
+  info(`fetch: ${event.request.url}`);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request)
+          .then((response) => {
+            const responseClone = response.clone();
+            caches.open("v1").then((cache) => {
+              cache.put(event.request, responseClone);
+            });
+            return response;
+          })
+          .catch(() => caches.match(`${path}/images/leaves.jpg`));
+      }
+    })
+  );
+});
